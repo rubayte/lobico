3af7d70d843e53b2e47f5a16dba60a0aa2235062
rubayte-nki <r.rahman@nki.nl>
2016-04-06 10:50:07 +0200

    data file updated and input diagram model updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
 //= require datatables.min
 //= require d3.v3.min
 //= require box
